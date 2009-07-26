05e41da60d5bb4f25a22df539f8a87a8def63041
pault414 <pault414@5c0b9abe-7a17-11de-90d1-ab2bd886e127>
2009-07-26 23:08:50 +0000

    fix avi recording

--- trunk/aviout.h
+++ trunk/aviout.h
@@ -6,16 +6,11 @@
 #include "windows.h"
 #include "mednafen.h"
 
-#ifdef __cplusplus
-extern "C" {
-#endif
 int DRV_AviIsRecording();
 bool DRV_AviBegin(const char* fname);
 
 void DRV_AviEnd();
 void DRV_AviVideoUpdate(const uint16* buffer, EmulateSpecStruct *espec);
 void DRV_AviSoundUpdate(void* soundData, int soundLen);
-#ifdef __cplusplus
-}
-#endif
+
 #endif
