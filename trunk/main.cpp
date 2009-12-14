403aa7acd423448c33a58404abb8e38bcc8a8d35
andres.delikat <andres.delikat@5c0b9abe-7a17-11de-90d1-ab2bd886e127>
2009-12-14 05:06:32 +0000

    Added my very snazzy icon, and successfully put it into the about box.  Loading it as the icon failed but probably because it is a bitmap file not an icon file.

--- trunk/main.cpp
+++ trunk/main.cpp
@@ -95,7 +95,7 @@ int WINAPI WinMain( HINSTANCE hInstance,
 	winClass.style = CS_HREDRAW | CS_VREDRAW;
 	winClass.lpfnWndProc = WndProc;
 	winClass.hInstance = hInstance;
-//	winClass.hIcon = LoadIcon(hInstance, (LPCTSTR)IDI_DIRECTX_ICON);
+	winClass.hIcon = LoadIcon(hInstance, "IDB_BITMAP1");
 //	winClass.hIconSm = LoadIcon(hInstance, (LPCTSTR)IDI_DIRECTX_ICON);
 	winClass.hCursor = LoadCursor(NULL, IDC_ARROW);
 	winClass.lpszMenuName = MAKEINTRESOURCE(IDC_CV);
