cb3e3eb8185fef606b6a251203bcc1920637ecda
kylethomson@gmail.com <kylethomson@gmail.com@5c0b9abe-7a17-11de-90d1-ab2bd886e127>
2010-09-25 16:33:34 +0000

    Added instructions and a user edit section to the Multitrack.lua script, should make the entire experience more modifiable to the TASer, as well as user friendly.

--- trunk/scripts/Multitrack.lua
+++ trunk/scripts/Multitrack.lua
@@ -1,8 +1,30 @@
 --PCEMultiTrack
+
+--***************************************************************************************
+--USER EDIT SECTION
+
+--           Up 1    Down 1    None   All   C1        C2         C3       C4         C5
+keymat = {'pageup','pagedown','N',   'A', 'numpad1','numpad2','numpad3','numpad4','numpad5'}
+
+-- Controller Input
+--               Up   Down   Left   Right   I  II   Run  Select
+local tabinp = {'up','down','left','right','Z','X','C',  'V'};
+
+--Names of Controllers
+--Example:
+--NameMatrix = {'Fighter','Warlock','Elf','Bard','Thief','All','None'};
+NameMatrix = {'C1','C2','C3','C4','C5','All','None'};
+
+--END USER EDIT SECTION
+--****************************************************************************************
+
+
 agg.setFont("verdana12_bold")		
-
-
 Buttons = {"Up","Down","Left","Right","I","II","Run","Select"};
+print("Welcome to DarkKobold's Multitrack Script!");
+print("To use, select controller you wish to record, with either the number pad (1-5) or pageup and pagedown. (A) selects all controllers, and (N) selects no controllers.");
+print("To change mapping, see the user edit section in the code.");
+
 AllFrames = {};
 local i = {};
 local inp ={};
@@ -10,31 +32,31 @@ local keys = {};
 
 local StartFrame = emu.framecount();
 local RecFrames = 0;
-currRec = 1;
-local tabinp = {'up','down','left','right','Z','X','C','V'};
+currRec = 7;
+
 
 function before()
 	keys = input.get();
 	RF = {};		
-	if keys['pageup'] then
+	if keys[keymat[1]] then
 		currRec = currRec + 1;
 		if currRec > 4 then currRec = 1; end;
-	elseif keys['pagedown'] then
+	elseif keys[keymat[2]] then
 		currRec = currRec - 1;
 		if currRec == 0 then currRec = 4; end;		
-	elseif keys['N'] then
+	elseif keys[keymat[3]] then
 		currRec = 7;
-	elseif keys['A'] then
+	elseif keys[keymat[4]] then
 		currRec = 6;
-	elseif keys['numpad1'] then
+	elseif keys[keymat[5]] then
 		currRec = 1;
-	elseif keys['numpad2'] then
+	elseif keys[keymat[6]] then
 		currRec = 2;
-	elseif keys['numpad3'] then
+	elseif keys[keymat[7]] then
 		currRec = 3;
-	elseif keys['numpad4'] then
+	elseif keys[keymat[8]] then
 		currRec = 4;
-	elseif keys['numpad5'] then
+	elseif keys[keymat[9]] then
 		currRec = 5;	
 	end;
 	if movie.mode() ~= 'playback' then							
@@ -80,8 +102,8 @@ function before()
 end;
 			
 gui.register(function()
-	agg.lineColor(255,255,255,255);	
-	agg.text(1,15,"Recording: " .. currRec);
+	agg.lineColor(255,255,255,255);		
+	agg.text(1,15,"Recording: " .. NameMatrix[currRec]);
 end);
 
 
